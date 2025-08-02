6745014b658a9c3b26fc3952a7c500de6d05bd4f
Jason <extremewinds@gmail.com>
2025-08-02 10:04:13 +0200

    include origin on port 8080

--- src/main/java/org/example/websocketchat/config/Config.java
+++ src/main/java/org/example/websocketchat/config/Config.java
@@ -11,17 +11,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class Config implements WebSocketMessageBrokerConfigurer {
 
 
-     @Override
-      public void registerStompEndpoints(StompEndpointRegistry registry) {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:8080")
                 .withSockJS();
-   }
+    }
 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
-   }
+    }
 }
